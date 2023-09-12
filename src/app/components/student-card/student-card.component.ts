ac6c50f27a625601cd5bdb2d321fa786f2518130
Ceccio03 <76108781+Ceccio03@users.noreply.github.com>
2023-09-12 12:53:51 +0200

    model

--- src/app/components/student-card/student-card.component.ts
+++ src/app/components/student-card/student-card.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { Student } from 'src/app/model/student';
 
 @Component({
   selector: 'app-student-card',
@@ -6,5 +7,5 @@ import { Component, Input } from '@angular/core';
   styleUrls: ['./student-card.component.scss']
 })
 export class StudentCardComponent {
-  @Input() studentData?:string;
+  @Input() studentData?:Student;
 }
